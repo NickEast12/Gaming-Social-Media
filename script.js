d98112e32406a06d15a767766b2e3a0f48cd6bc8
NickEast12 <nickeast1998@gmail.com>
2018-11-30 12:38:49 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const signInTrigger = document.getElementById('sign-in-opener');
+const signInClose = document.getElementById('sign-in-close');
+const overlay = document.getElementById('overlay');
+const signInBtn = document.getElementById('sign-in-btn');
+const incPassword = document.getElementById('incPassword');
+
+
+
+
+signInTrigger.addEventListener('click', () => {
+    overlay.style.transform = "translateY(0)";
+})
+signInClose.addEventListener('click', () => {
+    overlay.style.transform = "translateY(100%)"; 
+})
+
+
+
+    signInBtn.addEventListener('click' , () => {
+        const userName = document.getElementById('username').value;
+        const passWord = document.getElementById('password').value;
+        if (userName == 'username' && passWord == 'password') {
+            console.log(userName + passWord);
+            window.location.href = "main-page.html";
+        }else {
+            incPassword.innerHTML = 'Incorrect Username or Password'
+        }
+        
+        
+    })
+  
+
+  
+
+  
