77f1ba0ab5e505230caf24d5aa713293583c5deb
NickEast12 <nickeast1998@gmail.com>
2019-01-10 14:48:34 +0000

    comments under the main script page for future refrence

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// firstly assigning variable to the button on the main page
 const regClose = document.getElementById('regClose');
 const regBody = document.getElementById('registrationWrapper');
 const regOpen = document.getElementById('register-button');
-
+// adding event listeners to the buttons to slide in menus
 regOpen.addEventListener('click', () => {
     regBody.style.transform = "translateY(0%)";
 })
@@ -9,14 +10,14 @@ regClose.addEventListener('click', () => {
     regBody.style.transform = "translateY(-100%)";
 })
 
-
+// creating global variables for the registration page
 let regFirstName;
 let lastName;
 let regLastName;
 let email;
 let username;
 let passworD;
-
+// event listener for to collect data from inputs and close the registration page
 const regSubmit = document.getElementById('reg-submit');
 regSubmit.addEventListener('click', () => {
     regFirstName = document.getElementById('firstName').value;
@@ -28,8 +29,8 @@ regSubmit.addEventListener('click', () => {
     regBody.style.transform = "translateY(-100%)";
 })
 
-
-
+// same again for the sign in menu - thing to note here is the username and password 
+// variables have been pass to the sign in page by using a if statment 
 const signInTrigger = document.getElementById('sign-in-opener');
 const signInClose = document.getElementById('sign-in-close');
 const overlay = document.getElementById('overlay');
